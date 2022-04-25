36f9d3583e27fd41d35b51c009fe38818f304f6e
Steffen568 <sgonzalez568@gmail.com>
2022-04-25 18:40:56 -0500

    connected node to database. created mysql query for rows

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001
 const app = express()
+const mysql = require('mysql2')
 
 // middlewear
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// sends get request, displays hello world on browser
-app.get('/', (req, res) => {
-    res.json({
-        message: 'hello world'
-    })
+// connecting to mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //  Your mysql username
+        user: 'root',
+        // your mysql password
+        password: 'Htchc568!',
+        database: 'election'
+    },
+    console.log('connected to election database')
+)
+
+// db objecting using query method running sql and executes callback with all resulting rows
+db.query(`SELECT * FROM canidates`, (err, rows) => {
+    console.log(rows)
 })
 
 // default response for any other requests
