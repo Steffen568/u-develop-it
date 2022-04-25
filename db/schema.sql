c7312702a5228b7fe74169d9fb752a10ced2ac22
Steffen568 <sgonzalez568@gmail.com>
2022-04-25 17:52:20 -0500

    added mysql created sql files and gen election table

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE canidates (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    industry_connected VARCHAR(30) NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO canidates (first_name, last_name, industry_connected)
+VALUES 
+('Roland', 'Firbank', 1),
+('Virginia', 'Wolf', 1),
+('Piers', 'Gaveston', 0),
+('Charles', 'LeRoi', 1),
+('Katherine', 'Mansfield', 1),
+('Dora', 'Carrington', 0),
+('edward', 'Bellamy', 0),
+('Montague', 'Summers', 1),
+('Octovia', 'Butler', 2),
+('Unica', 'Zurn', 1);
